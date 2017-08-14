93e337063c631369ae3e746eb973204d97fc7b42
Clauss Franco <you@example.com>
2017-08-14 17:04:11 -0500

    agregando habilidades

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var cargarPagina = function(){
 	$('.modal').modal(); 
 	$(".button-collapse").sideNav();
+
+	var options = [
+    {selector: '#habilidadJS', offset: 200, callback: '$(this).addClass("animated")' },
+];
+Materialize.scrollFire(options);
 };
 
 $(document).ready(cargarPagina);
+
