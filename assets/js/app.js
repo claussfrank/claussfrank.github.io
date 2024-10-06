23c37f1724750f2b7be7ae140bbef6d1249f88d4
claussfrank <clauss.frank@gmail.com>
2024-10-06 01:37:44 -0600

    Ajustes
    Ajustes de  archivos

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// var cargarPagina = function(){
+// 	$('.modal').modal(); 
+// 	$(".button-collapse").sideNav();
+
+// 	var options = [
+//     {selector: '#habilidadJS', offset: 200, callback: '$(this).addClass("animated")' },
+// ];
+// Materialize.scrollFire(options);
+// };
+
+// $(document).ready(cargarPagina);
+
+// $(document).ready(function() {
+//   $("#navMenu").click(function() {
+//    //$(".nav").toggleClass("small");
+//     if ($(".nav").hasClass("small")) {
+//       $(".nav").removeClass("small");
+//     } else {
+//       $(".nav").addClass("small");
+//     }
+//   });
+// });
